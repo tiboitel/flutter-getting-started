58effd9113a45c7471b5014dfd3f12afb6e5feb8
tiboitel <tiboitel@student.42.fr>
2023-09-21 16:51:42 +0200

    feat: Add a favorite page that list all the favorited elements.

--- lib/main.dart
+++ lib/main.dart
@@ -71,7 +71,7 @@ class _MyHomePageState extends State<MyHomePage> {
         page = GeneratorPage();
         break;
       case 1:
-        page = Placeholder();
+        page = FavoritesPage();
         break;
       default:
         throw UnimplementedError('no widget for $selectedIndex');
@@ -186,6 +186,35 @@ class EvildDemonLordLabel extends StatelessWidget {
         child: Text(evilDemonLordName, style: style, semanticsLabel: evilDemonLordName,),
 
       ),
+    );
+  }
+}
+
+class FavoritesPage extends StatelessWidget { 
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+
+    if (appState.favorites.isEmpty) {
+      return Center (
+        child: Text('No favorites.'),     
+      );
+    }
+
+    return ListView(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(20),
+          child: Text('You have '
+          '${appState.favorites.length} favorites !'),
+        ),
+        for (var evilDemonLordName in appState.favorites)
+          ListTile(
+            leading: Icon(Icons.favorite),
+            title: Text(evilDemonLordName),
+          ),
+
+      ],
     );
   }
 }
